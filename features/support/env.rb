ed96129dad9211628dbe6be99d38231eea4a9383
emk9058 <emk9058@nyu.edu>
2025-11-07 14:54:10 -0500

    Add RSpec mocks support to Cucumber environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -51,3 +51,17 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
+require 'rspec/mocks'
+World(RSpec::Mocks::ExampleMethods)
+
+Before do
+  RSpec::Mocks.setup
+end
+
+After do
+  begin
+    RSpec::Mocks.verify
+  ensure
+    RSpec::Mocks.teardown
+  end
+end
