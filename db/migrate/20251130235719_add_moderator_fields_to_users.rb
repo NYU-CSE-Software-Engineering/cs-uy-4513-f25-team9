f42ad418930d31ecdaa813d898e5bf5138b2be7f
Raymond Lin <rl4713@nyu.edu>
2025-11-30 20:05:55 -0500

    Added failing cucumber tests for Moderation Remove Users feature

---
+++ db/migrate/20251130235719_add_moderator_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddModeratorFieldsToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :is_moderator, :boolean, default: false, null: false
+    add_column :users, :is_admin, :boolean, default: false, null: false
+  end
+end
