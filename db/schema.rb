b8574e84659cf3fe84e4c9d8ea0191d6dcc92892
anthony-lamelas <anthony.lamelas23@gmail.com>
2025-12-01 15:07:29 -0500

    fixed schema and CI workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_01_004021) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_01_025423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
