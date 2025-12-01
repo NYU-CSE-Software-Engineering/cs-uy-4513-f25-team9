79f66a01967a6911381dc0cd2a86e058c4497564
uriel-olayinka <umo204@nyu.edu>
2025-11-30 21:27:58 -0500

    Added failing BDD tests for messaging system

---
+++ features/step_definitions/messaging_steps.rb
@@ -0,0 +1,31 @@
+Given("I am logged in as a buyer") do
+  pending "Step implementation to be driven by TDD"
+end
+
+Given("I am viewing a product listing from another user") do
+  pending "Step implementation to be driven by TDD"
+end
+
+Given("I am on the message composition page for a product") do
+  pending "Step implementation to be driven by TDD"
+end
+
+When("I click {string}") do |button_text|
+  pending "Step implementation to be driven by TDD"
+end
+
+When("I fill in the message with {string}") do |message_content|
+  pending "Step implementation to be driven by TDD"
+end
+
+When("I click {string} without entering any text") do |button_text|
+  pending "Step implementation to be driven by TDD"
+end
+
+Then("I should see {string}") do |expected_text|
+  pending "Step implementation to be driven by TDD"
+end
+
+Then("I should see an error message {string}") do |error_message|
+  pending "Step implementation to be driven by TDD"
+end
