3bffc4eee5abf9607c65432317b7296e536d6847
uriel-olayinka <umo204@nyu.edu>
2025-12-09 12:37:22 -0500

    Added front end for messaging homepage as well as individual conversation on a listing, updated navbar with messaging navigation, fixed issue where you could message yourself about your own listing with validation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -7,4 +7,13 @@ class Conversation < ApplicationRecord
   validates :buyer, presence: true
   validates :seller, presence: true
   validates :listing, presence: true
+  validate :buyer_and_seller_are_different
+
+  private
+
+  def buyer_and_seller_are_different
+    if buyer_id == seller_id
+      errors.add(:seller, "Buyer and seller cannot be the same user")
+    end
+  end
 end
